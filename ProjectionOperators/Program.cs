4f9ec48dacd7ac1db09a0d57f0033b7ce651d9cb
ManuRamy <Manjunath.sampangi@gmail.com>
2022-08-28 19:26:17 +0530

    commit

--- ProjectionOperators/Program.cs
+++ ProjectionOperators/Program.cs
@@ -7,10 +7,21 @@ using System.Threading.Tasks;
 
 namespace ProjectionOperators
 {
+    class Employee
+	{
+		public int EmployeeID { get; set; }
+		public string Name { get; set; }
+		public int Salary { get; set; }
+	}
     class Program
     {
         static void Main(string[] args)
         {
+
+            /*Remember that if you want select all columns or single column then we dont use anonmyous type.
+             We should use anonymous type only if we want more than one column. IMP - inside anonymous, it should be assigned to some variable
+            or it should directly come from classobject.member*/
+
             #region Retrieves just the EmployeeID property of all employees
             /*
             DataTable dt = GetAllEmployees();
@@ -86,18 +97,50 @@ namespace ProjectionOperators
                  */
             #endregion
 
+            #region inside anonymous, it should be assigned to some variable or it should directly come from classobject.member
+            /*
+            List<Employee> employees = new List<Employee>
+			{
+				new Employee { EmployeeID = 1, Name = "Suresh",Salary=8000},
+				new Employee { EmployeeID = 2, Name = "Kiran", Salary=12000},
+				new Employee { EmployeeID = 3, Name = "Nitesh", Salary = 15000 }
+			};
+
+            var result = employees.Where(emp => emp.Salary > 12000).
+                                   Select(emp => new
+                                   {
+                                       emp.EmployeeID, //inside anonymous, it should be assigned to some variable or it should directly come from classobject.member
+                                       monthysalary = emp.Salary
+                                   });
+
+			foreach (var item in result)
+			{
+				Console.WriteLine($"The list is {item.EmployeeID} {item.monthysalary}");
+			}*/
+            #endregion
+
+
+            #region inside anonymous, using assigned to some variable
+            /*
             DataTable dt = GetAllEmployees();
-            var result = dt.AsEnumerable().Where(emp => emp.Field<int>("AnnualSalary") > 5000).Select(emp => new
-            {
-                Fullname = emp.Field<string>("FirstName") + emp.Field<string>("LastName"),
-                AnnualSalary = emp.Field<int>("AnnualSalary") * .1
-            });
+			//var result = dt.AsEnumerable().Where(emp => emp.Field<int>("AnnualSalary") > 45000).
+			//							   Select(emp => emp.Field<string>("FirstName")); //this is simple access
 
-            foreach (var v in result)
-            {
-                Console.WriteLine(v.Fullname + " - " + v.AnnualSalary);
-            }
 
+			var result = dt.AsEnumerable().Where(emp => emp.Field<int>("AnnualSalary") > 45000).
+													Select(emp => new
+													{
+														name = emp.Field<string>("FirstName"), //inside anonymous, here we using assigned to some variable
+														gender = emp.Field<string>("Gender")
+													});
+
+			foreach (var v in result)
+			{
+				Console.WriteLine($"The employeename is {v}");
+			}
+            */
+            #endregion
+            Console.ReadLine();
         }
 
         public static DataTable GetAllEmployees()
